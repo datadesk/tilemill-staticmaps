8b741995b85eb542598b69240026b9f1b908799c
Ken Schwencke <schwank@gmail.com>
2013-05-02 13:07:14 -0700

    lon/lat bro

--- configure.py
+++ configure.py
@@ -24,7 +24,7 @@ default = os.path.join(home, 'Documents', 'MapBox')
 project_dir = os.path.join(os.environ.get('TILEMILL_PROJECTS', default), 'project')
 tmp_location = "/tmp/staticmap_geojson.json"
 
-def prep_mml_layer(lat, lon):
+def prep_mml_layer(lon, lat):
     """
     Takes a lat/lon and writes it to a tmp geojson template.
     """
@@ -32,7 +32,7 @@ def prep_mml_layer(lat, lon):
     
 
     with open(tmp_location, "w") as f:
-        f.write(geojson_tmpl % (lat, lon))
+        f.write(geojson_tmpl % (lon, lat))
     f.closed
     
     return {
@@ -51,7 +51,7 @@ def prep_mml_layer(lat, lon):
 
 
 
-def doit(project_mml, lat, lon):
+def doit(project_mml, lon, lat):
     with open(project_mml + ".template", 'r') as f:
       newf = json.loads(f.read())
     f.closed
@@ -62,7 +62,7 @@ def doit(project_mml, lat, lon):
         if feat_caching:
             layer["properties"]["cache-features"] = "true"
       #Drop the new single-point MML in.
-      newf["Layer"].insert(0, prep_mml_layer(lat, lon))
+      newf["Layer"].insert(0, prep_mml_layer(lon, lat))
       f.write(json.dumps(newf, sort_keys=True, indent=2))
     f.closed
 
@@ -70,6 +70,6 @@ if __name__ == "__main__":
     project, point = sys.argv[1:]
     if not '_staticmaps' in project:
        project = project + '_staticmaps'
-    lat, lon = point.split(',')
+    lon, lat = point.split(',')
     target_mml = os.path.join(project_dir, project, 'project.mml')
-    doit(target_mml, lat, lon)
+    doit(target_mml, lon, lat)

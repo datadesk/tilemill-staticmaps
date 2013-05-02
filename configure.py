f4c6b971f4e62f4657dd4cfa85adafc0d4051e4c
Ken Schwencke <schwank@gmail.com>
2013-05-02 15:50:18 -0700

    Added bounding box and stylsheet to configure

--- configure.py
+++ configure.py
@@ -15,6 +15,7 @@ feat_caching = True
 #
 
 import os
+import math
 import json
 import sys
 from os.path import join
@@ -23,6 +24,33 @@ home = os.environ.get('HOME')
 default = os.path.join(home, 'Documents', 'MapBox')
 project_dir = os.path.join(os.environ.get('TILEMILL_PROJECTS', default), 'project')
 tmp_location = "/tmp/staticmap_geojson.json"
+
+def get_bounding_box(longitude_in_degrees, latitude_in_degrees, half_side_in_miles, half_height_in_miles=None):
+    if not half_height_in_miles:
+        half_height_in_miles = half_side_in_miles
+
+    half_side_in_km = half_side_in_miles * 1.609344
+    half_height_in_km = half_height_in_miles * 1.609344
+    lat = math.radians(latitude_in_degrees)
+    lon = math.radians(longitude_in_degrees)
+
+    radius  = 6371
+    # Radius of the parallel at given latitude
+    parallel_radius = radius*math.cos(lat)
+
+    lat_min = lat - half_height_in_km/radius
+    lat_max = lat + half_height_in_km/radius
+    lon_min = lon - half_side_in_km/parallel_radius
+    lon_max = lon + half_side_in_km/parallel_radius
+    rad2deg = math.degrees
+
+    return (
+        rad2deg(lon_min),
+        rad2deg(lat_min),
+        rad2deg(lon_max),
+        rad2deg(lat_max),
+    )
+
 
 def prep_mml_layer(lon, lat):
     """
@@ -37,15 +65,15 @@ def prep_mml_layer(lon, lat):
     
     return {
         "geometry": "point",
-        #"extent": [],
-        "id": "staticmap-dynamic",
+        "extent": get_bounding_box(lon, lat, 0.5),
         "class": "",
         "Datasource": {
             "file": tmp_location
             },
-        "srs-name": "autodetect",
         "srs": "",
+        "srs-name": "autodetect",
         "advanced": {},
+        "id": "staticmap-dynamic",
         "name": "staticmap-dynamic"
     }
 
@@ -62,7 +90,8 @@ def doit(project_mml, lon, lat):
         if feat_caching:
             layer["properties"]["cache-features"] = "true"
       #Drop the new single-point MML in.
-      newf["Layer"].insert(0, prep_mml_layer(lon, lat))
+      newf["Layer"].append(prep_mml_layer(lon, lat))
+      newf["Stylesheet"].append("staticmap-dynamic.mss")
       f.write(json.dumps(newf, sort_keys=True, indent=2))
     f.closed
 
@@ -70,6 +99,6 @@ if __name__ == "__main__":
     project, point = sys.argv[1:]
     if not '_staticmaps' in project:
        project = project + '_staticmaps'
-    lon, lat = point.split(',')
+    lon, lat = map(float, point.split(','))
     target_mml = os.path.join(project_dir, project, 'project.mml')
     doit(target_mml, lon, lat)

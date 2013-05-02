3bfa3da79d99cdb460dd073bb7f90b222cc46c17
Ken Schwencke <schwank@gmail.com>
2013-05-02 15:49:32 -0700

    Added a quick and dirty stylesheet to clone_project.sh

--- clone_project.sh
+++ clone_project.sh
@@ -4,4 +4,5 @@ echo Cloning project $1 as $1_staticmaps
 : ${TILEMILL_PROJECTS:="$HOME/Documents/MapBox"}
 cp -a $TILEMILL_PROJECTS/project/$1/ $TILEMILL_PROJECTS/project/$1_staticmaps
 cp -a $TILEMILL_PROJECTS/project/$1/_staticmaps/project.mml $TILEMILL_PROJECTS/project/$1_staticmaps/project.mml.template
+echo "#staticmap-dynamic{  marker-width:8;  marker-fill:#f45;  marker-line-color:#813;  marker-allow-overlap:true;  marker-line-width:0.5;}" > $TILEMILL_PROJECTS/project/$1_staticmaps/staticmap-dynamic.mss
 

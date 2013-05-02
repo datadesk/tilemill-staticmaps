a19b6353a2a1cf9b141414d17cc022e213757c80
Ken Schwencke <schwank@gmail.com>
2013-05-02 15:55:38 -0700

    typo in clone

--- clone_project.sh
+++ clone_project.sh
@@ -3,6 +3,6 @@ echo Cloning project $1 as $1_staticmaps
 # This checks for TILEMILL_PROJECTS and sets it to a default if it's not set.
 : ${TILEMILL_PROJECTS:="$HOME/Documents/MapBox"}
 cp -a $TILEMILL_PROJECTS/project/$1/ $TILEMILL_PROJECTS/project/$1_staticmaps
-cp -a $TILEMILL_PROJECTS/project/$1/_staticmaps/project.mml $TILEMILL_PROJECTS/project/$1_staticmaps/project.mml.template
+cp -a $TILEMILL_PROJECTS/project/$1_staticmaps/project.mml $TILEMILL_PROJECTS/project/$1_staticmaps/project.mml.template
 echo "#staticmap-dynamic{  marker-width:8;  marker-fill:#f45;  marker-line-color:#813;  marker-allow-overlap:true;  marker-line-width:0.5;}" > $TILEMILL_PROJECTS/project/$1_staticmaps/staticmap-dynamic.mss
 
